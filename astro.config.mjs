92dfecb07efc029cb7de3ac73283a952c36f2ba8
LbsLightX <shivamkumarking66@gmail.com>
2025-12-28 21:24:10 +0530

    🔥 | fix: switch astro config from server to static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vue from '@astrojs/vue';
 
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  site: import.meta.env.PROD ? 'https://alyxia.dev' : 'http://localhost:4321',
-  integrations: [mdx(), sitemap(), vue()]
+  site: 'https://lbslightx.github.io/voyeur/',
+  base: '/voyeur/',
+  integrations: [mdx(), sitemap(), vue()],
 });
