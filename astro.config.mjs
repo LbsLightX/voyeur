8fba39b603c4f41de008ad0644e614e237a6b75c
LbsLightX <shivamkumarking66@gmail.com>
2025-12-28 23:34:55 +0530

    ⚛️ | feat: enable Vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
 
 export default defineConfig({
   site: "https://lbslightx.github.io",
   base: "/voyeur/",
+  integrations: [vue()]
 });
